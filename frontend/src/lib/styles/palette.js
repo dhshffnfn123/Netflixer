d2f75798f81daab91dd05ab9b8855f2cdc13db49
B H An <anbyunghoon0107@gamil.com>
2021-12-23 13:28:01 +0900

    글로벌 css, 버튼 css 적용

---
+++ frontend/src/lib/styles/palette.js
@@ -0,0 +1,7 @@
+const palette = {
+  backgroundColor: '#343a40',
+  inputBoxColor: '#868e96',
+  fontColor: '#c92a2a',
+};
+
+export default palette;
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <div>
+      <Button>버튼</Button>
+    </div>
+  );
 };
 
 export default MainPage;
