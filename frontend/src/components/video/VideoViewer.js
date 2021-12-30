2df9d492530a573c1770df037aada7206c667a8b
B H An <anbyunghoon0107@gamil.com>
2021-12-30 22:40:26 +0900

    Video 조회페이지 구현 중

--- frontend/src/components/video/VideoViewer.js
+++ frontend/src/components/video/VideoViewer.js
@@ -5,12 +5,33 @@ import Responsive from '../common/Responsive';
 
 const VideoViewerBlock = styled(Responsive)`
   /* background: ${palette.backgroundColor}; */
+  margin-top: 5rem;
   background: gray;
   display: grid;
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-rows: 1fr 1fr;
+  }
+`;
+const VideoPoster = styled.div`
+  height: 800px;
+  background: cornflowerblue;
+`;
+const VideoInfo = styled.div`
+  background: seagreen;
 `;
 
 const VideoViewer = () => {
-  return <VideoViewerBlock></VideoViewerBlock>;
+  return (
+    <VideoViewerBlock>
+      <VideoPoster />
+      <VideoInfo>
+        <h1>TITLE</h1>
+      </VideoInfo>
+    </VideoViewerBlock>
+  );
 };
 
 export default VideoViewer;
