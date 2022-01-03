a386fd42bd41914f298516d997b1aaf84ec9d7a6
B H An <anbyunghoon0107@gamil.com>
2022-01-03 17:02:14 +0900

    비디오뷰어 페이지 반응형 구현 완료

--- frontend/src/components/video/VideoViewer.js
+++ frontend/src/components/video/VideoViewer.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
-import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
 
 const VideoViewerBlock = styled(Responsive)`
-  /* background: ${palette.backgroundColor}; */
   margin-top: 5rem;
   display: grid;
-  @media (min-width: 768px) {
-    grid-template-columns: 661px 1fr;
+  grid-template-columns: 661px 1fr;
+  @media (max-width: 1500px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 707px;
+  }
+  @media (max-width: 1024px) {
+    grid-template-columns: 1fr 368px;
+    grid-template-rows: 525px;
   }
   @media (max-width: 768px) {
-    grid-template-rows: 1fr 1fr;
+    display: flex;
+    flex-wrap: wrap;
+    margin: auto;
+    margin-top: 5rem;
   }
 `;
 const VideoPoster = styled.div`
-  height: 800px;
   background-image: url('/img/deadfool.png');
   background-repeat: no-repeat;
   background-size: contain;
   margin-left: 100px;
   box-shadow: 1px 1px 10px black;
+  @media (max-width: 1500px) {
+    margin: 0;
+  }
+  @media (max-width: 768px) {
+    background-size: contain;
+    width: 400px;
+    height: 570px;
+    margin: auto;
+  }
 `;
 const VideoInfo = styled.div`
   padding-left: 100px;
+  @media (max-width: 768px) {
+    width: 400px;
+    padding-left: 0;
+    margin: auto;
+    margin-top: 20px;
+  }
 `;
 const Title = styled.p`
   color: #f8f9fa;
@@ -56,6 +77,17 @@ const Summary = styled.div`
   height: 100px;
   color: #ced4da;
   font-size: 1.2rem;
+
+  @media (max-width: 1500px) {
+    font-size: 1rem;
+  }
+  @media (max-width: 1024px) {
+    margin-bottom: 150px;
+  }
+  @media (max-width: 768px) {
+    margin-top: 20px;
+    margin-bottom: 10px;
+  }
 `;
 const Director = styled.p`
   font-size: 1rem;
@@ -66,19 +98,69 @@ const Director = styled.p`
 const StillCutText = styled.p`
   margin: 0;
   font-size: 2rem;
-  margin-top: 80px;
+  margin-top: 190px;
   margin-bottom: 10px;
   color: #ced4da;
   text-shadow: 0 0 5px gray;
+
+  @media (max-width: 1500px) {
+    margin-top: 100px;
+  }
+  @media (max-width: 1024px) {
+    font-size: 1.5rem;
+    position: relative;
+    right: 470px;
+    margin-top: 70px;
+  }
+  @media (max-width: 768px) {
+    position: unset;
+    margin-top: 30px;
+  }
 `;
 const StillCut = styled.div`
   height: 200px;
-  background: white;
+  background: transparent;
   display: flex;
   justify-content: space-between;
+
   div {
-    width: 230px;
+    width: 100%;
     background: gray;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: 0 0 10px black;
+  }
+
+  @media (max-width: 1500px) {
+    div:nth-child(3) {
+      display: none;
+    }
+  }
+  @media (max-width: 1024px) {
+    position: relative;
+    right: 470px;
+    width: 760px;
+    div:nth-child(3) {
+      display: block;
+    }
+    div {
+      width: 400px;
+    }
+  }
+  @media (max-width: 768px) {
+    position: unset;
+    width: 400px;
+    margin-bottom: 100px;
+    background-size: contain;
+    div {
+      height: 250px;
+    }
+    div:nth-child(3) {
+      display: none;
+    }
+    div:nth-child(1) {
+      display: none;
+    }
   }
 `;
 
@@ -103,9 +185,9 @@ const VideoViewer = () => {
         </Director>
         <StillCutText>Still Cut_</StillCutText>
         <StillCut>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
+          <div style={{ backgroundImage: 'url("/img/still/deadfool1.jpg")' }} />
+          <div style={{ backgroundImage: 'url("/img/still/deadfool2.jpg")' }} />
+          <div style={{ backgroundImage: 'url("/img/still/deadfool3.jpg")' }} />
         </StillCut>
       </VideoInfo>
     </VideoViewerBlock>
