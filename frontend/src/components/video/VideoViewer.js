c408f2d300b521bf839a889a54a0d279e7c563b7
B H An <anbyunghoon0107@gamil.com>
2022-01-04 16:56:00 +0900

    리뷰 박스 코드 세분화

--- frontend/src/components/video/VideoViewer.js
+++ frontend/src/components/video/VideoViewer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../common/Responsive';
+import Button from '../common/Button';
+
+/* ------------------------------- VideoViewer ------------------------------ */
 
 const VideoViewerBlock = styled(Responsive)`
   margin-top: 5rem;
@@ -139,7 +142,7 @@ const StillCut = styled.div`
   @media (max-width: 1024px) {
     position: relative;
     right: 470px;
-    width: 760px;
+    width: 735px;
     div:nth-child(3) {
       display: block;
     }
@@ -167,7 +170,64 @@ const StillCut = styled.div`
 /* --------------------------------- review --------------------------------- */
 
 const ReviewBlock = styled(Responsive)`
-  background: white;
+  margin-top: 100px;
+  padding-bottom: 5rem;
+  display: flex;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    margin-top: 0;
+  }
+`;
+const ReviewButton = styled(Button)`
+  position: unset;
+  width: 100px;
+  margin-left: 100px;
+  height: 100px;
+  border-radius: 0;
+  font-size: 0.8rem;
+  font-weight: 800;
+  background: #dddddd;
+  &:hover {
+    border: 2px solid #640000;
+  }
+
+  @media (max-width: 1500px) {
+    margin-left: 0;
+  }
+  @media (max-width: 1024px) {
+    margin-top: 200px;
+  }
+  @media (max-width: 768px) {
+    margin-top: 0;
+    margin: auto;
+    width: 400px;
+    height: 50px;
+    font-size: 1.3rem;
+  }
+`;
+const ReviewInput = styled.input`
+  margin-left: 20px;
+  width: 100%;
+  background: transparent;
+  border: 3px solid #444444;
+  outline: none;
+  color: #bbbbbb;
+  font-size: 1.5rem;
+  word-wrap: break-word;
+  transition: 0.3s all ease;
+  &:focus {
+    border: 3px solid #640000;
+  }
+  @media (max-width: 1024px) {
+    margin-top: 200px;
+  }
+  @media (max-width: 768px) {
+    margin: auto;
+    margin-top: 10px;
+    width: 400px;
+    height: 50px;
+    font-size: 0.7rem;
+  }
 `;
 
 const VideoViewer = () => {
--- frontend/src/pages/VideoPage.js
+++ frontend/src/pages/VideoPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import VideoViewer from '../components/video/VideoViewer';
+import ReviewBox from '../components/video/ReviewBox';
 
 const VideoPage = () => {
   return (
     <>
       <HeaderContainer />
       <VideoViewer />
+      <ReviewBox />
     </>
   );
 };
