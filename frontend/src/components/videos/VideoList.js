4febff22c3720d6dae2eb0c93dd2c552607a3eef
B H An <anbyunghoon0107@gamil.com>
2022-01-15 22:49:00 +0900

    메인페이지 UI 수정 중

--- frontend/src/components/videos/VideoList.js
+++ frontend/src/components/videos/VideoList.js
@@ -2,45 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../common/Responsive';
 import palette from '../../lib/styles/palette';
-import SubInfo from '../common/SubInfo';
 import Tags from '../common/Tags';
 
 const VideoListBlock = styled(Responsive)`
   margin-top: 3rem;
   background: white;
   display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
 
 const VideoItemBlock = styled.div`
-  padding-top: 3rem;
-  padding-bottom: 3rem;
   background: gray;
   margin-bottom: 100px;
-  width: 300px;
+  width: 320px;
+  height: 530px;
 
-  &:first-child {
-  }
-
-  h2 {
-    font-size: 2rem;
-    margin-bottom: 0;
-    margin-top: 0;
-    &:hover {
-      color: ${palette.inputBoxColor};
-    }
-  }
   p {
-    margin-top: 2rem;
   }
+`;
+
+const PosterBox = styled.div`
+  width: 100%;
+  height: 455px;
+  background: cornflowerblue;
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+const Title = styled.p`
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 1.5rem;
+  font-weight: 500;
 `;
 
 const VideoItem = () => {
   return (
     <VideoItemBlock>
-      <h2>제목</h2>
-      <SubInfo />
+      <PosterBox style={{ backgroundImage: `url("/img/deadpool.png")` }} />
+      <Title>데드풀</Title>
       <Tags tags={['태그1', '태그2', '태그3']} />
-      <p>기타 정보를 넣는 곳</p>
     </VideoItemBlock>
   );
 };
@@ -48,6 +50,15 @@ const VideoItem = () => {
 const VideoList = () => {
   return (
     <VideoListBlock>
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
       <VideoItem />
       <VideoItem />
       <VideoItem />
