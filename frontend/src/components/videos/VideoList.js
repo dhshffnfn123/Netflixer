b53cf234939f8eb8066454c5efc3700cf4f17119
B H An <anbyunghoon0107@gamil.com>
2022-01-17 22:44:03 +0900

    메인 페이지 반응형 UI

--- frontend/src/components/videos/VideoList.js
+++ frontend/src/components/videos/VideoList.js
@@ -6,28 +6,45 @@ import Tags from '../common/Tags';
 
 const VideoListBlock = styled(Responsive)`
   margin-top: 3rem;
-  background: white;
+  background: transparent;
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
+
+  @media (max-width: 768px) {
+    width: 550px;
+  }
 `;
 
 const VideoItemBlock = styled.div`
-  background: gray;
+  background: #3e3e3e;
   margin-bottom: 100px;
+  box-shadow: black 0 0 10px;
   width: 320px;
   height: 530px;
 
-  p {
+  .box > * {
+    padding-left: 10px;
+  }
+
+  @media (max-width: 768px) {
+    width: 520px;
+    height: 300px;
+    display: grid;
+    grid-template-columns: 210px 1fr;
   }
 `;
 
 const PosterBox = styled.div`
   width: 100%;
   height: 455px;
-  background: cornflowerblue;
   background-repeat: no-repeat;
   background-size: contain;
+
+  @media (max-width: 768px) {
+    width: 210px;
+    height: 300px;
+  }
 `;
 
 const Title = styled.p`
@@ -35,14 +52,36 @@ const Title = styled.p`
   margin-bottom: 0;
   font-size: 1.5rem;
   font-weight: 500;
+  color: #b9b9b9;
+  padding-top: 5px;
+
+  @media (max-width: 768px) {
+  }
+`;
+
+const Summary = styled.div`
+  color: #b9b9b9;
+  padding-top: 25px;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
 `;
 
 const VideoItem = () => {
   return (
     <VideoItemBlock>
       <PosterBox style={{ backgroundImage: `url("/img/deadpool.png")` }} />
-      <Title>데드풀</Title>
-      <Tags tags={['태그1', '태그2', '태그3']} />
+      <div className="box">
+        <Title>데드풀</Title>
+        <Tags tags={['태그1', '태그2', '태그3']} />
+        <Summary>
+          세상에 이런 히어로쯤 하나 괜찮잖아? 비밀 실험에 참여한 후 특별한
+          능력을 갖게 된 주인공. 쌔끈한 빨간 슈트에 찰진 드립 장착한 '데드풀'로
+          거듭난다. 파워는 어마무시, 책임감은 개나 줘버려. 어디 한번 놀아보실까?
+          뮤직 큐.
+        </Summary>
+      </div>
     </VideoItemBlock>
   );
 };
