037fada00d4e10f562a591b1ba7333f28a32a354
B H An <anbyunghoon0107@gamil.com>
2022-02-05 23:25:04 +0900

    리뷰리스트컨테이너 작성

--- frontend/src/containers/reviews/ReviewListContainer.js
+++ frontend/src/containers/reviews/ReviewListContainer.js
@@ -1,1 +1,26 @@
 import React, { useEffect } from 'react';
+import qs from 'qs';
+import { withRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import ReviewList from '../../components/videos/VideoList';
+import { listReviews } from '../../modules/videos';
+
+const ReviewListContainer = ({ location, match }) => {
+  const dispatch = useDispatch();
+  const { reviews, error, loading, user } = useSelector(
+    ({ reviews, loading, user }) => ({
+      reviews: reviews.reviews,
+      error: reviews.error,
+      loading: loading['reviews/LIST_REVIEWS'],
+      user: user.user,
+    }),
+  );
+  useEffect(() => {
+    const { videoId } = match.params;
+    dispatch(listReviews({ videoId }));
+  }, [dispatch]);
+
+  return <ReviewList loading={loading} error={error} reviews={reviews} />;
+};
+
+export default withRouter(ReviewListContainer);
