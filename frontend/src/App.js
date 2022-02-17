1738e162e68dff8fbfcf11b2daa077a196caae87
B H An <anbyunghoon0107@gamil.com>
2022-02-17 22:03:53 +0900

    관리자 페이지 컴포넌트 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,12 @@ import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
 import VideoPage from './pages/VideoPage';
 import WritePage from './pages/WritePage';
+import AdminPage from './pages/AdminPage';
 
 const App = () => {
   return (
     <>
+      <Route component={AdminPage} path="/admin" />
       <Route component={MainPage} path="/" exact />
       <Route component={LoginPage} path="/login" />
       <Route component={RegisterPage} path="/register" />
---
+++ frontend/src/pages/AdminPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminPage = () => {
+  return <div>this is AdminPage</div>;
+};
+
+export default AdminPage;
