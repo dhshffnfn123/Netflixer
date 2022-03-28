ead11d88d2b00daa73559b917808538a57b4f29b
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-03-28 23:11:49 +0900

    chart component 생성 (커스텀 전)

--- frontend/src/components/admin/ManagementList.js
+++ frontend/src/components/admin/ManagementList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 import Button from '../common/Button';
 import Responsive from '../common/Responsive';
---
+++ frontend/src/components/common/Chart.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import {
+  ResponsiveContainer,
+  AreaChart,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Area,
+  CartesianGrid,
+} from 'recharts';
+// recharts 꺾은선 그래프 사용 예정
+
+const Chart = () => {
+  return (
+    <Container>
+      <></>
+    </Container>
+  );
+};
+export default Chart;
+
+const Container = styled.div``;
