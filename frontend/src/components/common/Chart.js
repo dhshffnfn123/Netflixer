ae1ead9ba6ac4a017aa0d2df03deb1780eef85b3
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-03-31 22:56:15 +0900

    차트 커스텀 중

--- frontend/src/components/common/Chart.js
+++ frontend/src/components/common/Chart.js
@@ -2,21 +2,31 @@ import styled from 'styled-components';
 import {
   ResponsiveContainer,
   LineChart,
-  ReferenceLine,
   XAxis,
   YAxis,
   Tooltip,
-  Area,
   CartesianGrid,
 } from 'recharts';
 // recharts 꺾은선 그래프 사용 예정
+const data = [];
+for (let i = 0; i < 30; i++) {
+  data.push({
+    reviews: Math.floor(Math.random() * 10),
+    day: i,
+  });
+}
+
+console.log(data);
 
 const Chart = () => {
   return (
     <Container>
       <ResponsiveContainer>
-        <LineChart>
-          <Tooltip />ㄴ
+        <LineChart margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
+          <XAxis />
+          <YAxis />
+          <CartesianGrid />
+          <Tooltip />
         </LineChart>
       </ResponsiveContainer>
     </Container>
