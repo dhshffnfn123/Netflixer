920cb43e5b26834f483114682a3a241a023265cc
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-04-02 20:25:48 +0900

    리뷰 리스트 크기 조정 완료

--- frontend/src/components/reviews/ReviewList.js
+++ frontend/src/components/reviews/ReviewList.js
@@ -3,39 +3,6 @@ import styled from 'styled-components';
 import Responsive from '../common/Responsive';
 import palette from '../../lib/styles/palette';
 
-const ReviewListBlock = styled(Responsive)`
-  margin-top: 1rem;
-  padding-bottom: 3rem;
-`;
-const ReviewItemBlock = styled.div`
-  @media (min-width: 1500px) {
-    margin-left: 100px;
-  }
-  @media (min-width: 768px) {
-    width: 400px;
-  }
-  margin-bottom: 1rem;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  background: ${palette.reviewListColor};
-  color: white;
-
-  span {
-    padding-left: 1rem;
-    font-size: 0.9rem;
-  }
-
-  p {
-    margin-top: 1rem;
-    padding-left: 1rem;
-    font-size: 1.2rem;
-  }
-
-  &:last-child {
-    margin-bottom: 2rem;
-  }
-`;
-
 const ReviewItem = ({ review }) => {
   const { publishedDate, user, text } = review;
   return (
@@ -69,3 +36,36 @@ const ReviewList = ({ reviews, loading, error }) => {
 };
 
 export default ReviewList;
+
+const ReviewListBlock = styled(Responsive)`
+  margin-top: 1rem;
+  padding-bottom: 3rem;
+`;
+const ReviewItemBlock = styled.div`
+  width: 100%;
+  @media (max-width: 768px) {
+    width: 400px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  margin-bottom: 1rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background: ${palette.reviewListColor};
+  color: white;
+
+  span {
+    padding-left: 1rem;
+    font-size: 0.9rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    padding-left: 1rem;
+    font-size: 1.2rem;
+  }
+
+  &:last-child {
+    margin-bottom: 2rem;
+  }
+`;
