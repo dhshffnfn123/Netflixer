525459574beabbd4623d6e31e76ee410b134ce16
B H An <anbyunghoon0107@gamil.com>
2022-04-06 23:13:54 +0900

    Page 생성

---
+++ frontend/src/pages/AddContentPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const AddContentPage = () => {
+  return (
+    <Container>
+      <></>
+    </Container>
+  );
+};
+
+export default AddContentPage;
+
+const Container = styled.div``;
---
+++ frontend/src/pages/MemberManagementPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MemberManagementPage = () => {
+  return (
+    <Container>
+      <></>
+    </Container>
+  );
+};
+
+export default MemberManagementPage;
+
+const Container = styled.div``;
