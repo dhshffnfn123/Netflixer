d03a10ed0a97b64b6fcf0a3eb91c63c0191a3b8b
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-04-09 17:32:20 +0900

    page말고 modal로 할거임

---
+++ frontend/src/components/admin/modal/MemberModal.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const MemberModal = () => {
+  return (
+    <FullScreen>
+      <Container>
+        <></>
+      </Container>
+    </FullScreen>
+  );
+};
+
+export default MemberModal;
+
+const FullScreen = styled.div``;
+const Container = styled.div``;
