97eb15c801548785da8cea2eafff2654970cb665
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-04-10 18:49:18 +0900

    modal base

--- frontend/src/components/admin/modal/MemberModal.js
+++ frontend/src/components/admin/modal/MemberModal.js
@@ -12,5 +12,20 @@ const MemberModal = () => {
 
 export default MemberModal;
 
-const FullScreen = styled.div``;
-const Container = styled.div``;
+const FullScreen = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 30;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Container = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: white;
+`;
