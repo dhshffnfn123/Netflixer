07f4b8cfc6d1a698268da1b78fc5adf59ebf0768
B H An <anbyunghoon0107@gamil.com>
2022-04-11 22:44:47 +0900

    modal UI 수정 중

--- frontend/src/components/admin/ManagerModal.js
+++ frontend/src/components/admin/ManagerModal.js
@@ -13,5 +13,10 @@ export default ManageModal;
 const FullScreen = styled.div`
   width: 100%;
   height: 100%;
+  background: #333333;
 `;
-const Container = styled.div``;
+const Container = styled.div`
+  width: 500px;
+  height: 500px;
+  background: white;
+`;
