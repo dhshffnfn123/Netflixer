97ca722f910695e3d8b159334b9611a9cd97a1c5
B H An <anbyunghoon0107@gamil.com>
2022-04-11 22:41:58 +0900

    modal 컴포넌트

--- frontend/src/components/admin/ManagerModal.js
+++ frontend/src/components/admin/ManagerModal.js
@@ -10,5 +10,8 @@ const ManageModal = () => {
 
 export default ManageModal;
 
-const FullScreen = styled.div``;
+const FullScreen = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 const Container = styled.div``;
