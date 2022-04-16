52e5ffe29c008aaa3a7e681dfe9ffd7267f8fe48
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-04-16 18:26:42 +0900

    멤버리스트 컴포넌트 생성

---
+++ frontend/src/components/admin/member/MemberList.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MemberList = () => {
+  return (
+    <Container>
+      <></>
+    </Container>
+  );
+};
+
+export default MemberList;
+
+const Container = styled.div``;
