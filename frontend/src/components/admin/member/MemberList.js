753f44ed075f7b3b87b6046bbbc21f02203cc611
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-05-22 15:05:54 +0900

    스타일 변경

--- frontend/src/components/admin/member/MemberList.js
+++ frontend/src/components/admin/member/MemberList.js
@@ -15,7 +15,7 @@ export default MemberList;
 const Container = styled.div`
   width: 100%;
   height: 20%;
-  border: solid 1px black;
+  border: solid 1px red;
   display: grid;
   grid-template-columns: 60% 1fr 1fr;
 `;
