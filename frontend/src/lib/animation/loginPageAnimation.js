659a102d21851392e94ad324ab98dc1b31ed77ac
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-06-13 21:16:24 +0900

    코드 정리

--- frontend/src/lib/animation/loginPageAnimation.js
+++ frontend/src/lib/animation/loginPageAnimation.js
@@ -1,20 +1,18 @@
 import { keyframes } from 'styled-components';
 
-const visibleLogo = keyframes`
+export const visibleLogo = keyframes`
     0% { opacity: 0; }
     50% { margin-top: 6rem; opacity: 1; }
     100% { margin-top: 0; opacity: 1; }
 `;
 
-const visibleAuthFromBlock = keyframes`
+export const visibleAuthFromBlock = keyframes`
     0% { opacity: 0; }
     100% { opacity: 1;}
 `;
 
-const visibleError = keyframes`
+export const visibleError = keyframes`
     0% { opacity: 1;}
     70% { opacity: 1;}
     100% { opacity: 0;}
 `;
-
-export { visibleLogo, visibleAuthFromBlock, visibleError };
